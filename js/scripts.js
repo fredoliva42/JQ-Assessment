0b57571297ad91e5429e21e0756dae967de8d364
Frederic Oliva <fredoliva42@gmail.com>
2017-04-12 19:22:46 -0700

    debbug and modify the scripts.js and index.html files

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,19 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    $("#suggestion").hide();
-    $("form").show();
+
+    $("#suggestion").show();
+    $("form").hide();
+
     event.preventDefault();
+
     var name = $("#name").val();
     var activity = $("#activity").val();
     var animal = $("#animal").val();
     var kids = $("#kids").val();
     var passeport = $("#passeport").val();
-
+    var destination;
     var suggestion = function(kids, animal, activity, passeport) {
-      if (kids === "false" && animal === "false" && activity === "gastronomy" && passeport === "true") || (kids === "true" && animal === "true" && activity === "gastronomy" && passeport === "true") {
+      if ((kids === "false" && animal === "false" && activity === "gastronomy" && passeport === "true") || (kids === "true" && animal === "true" && activity === "gastronomy" && passeport === "true")) {
         destination = "Europe";
       } else if (kids === "false" && animal === "false" && activity === "culture" && passeport === "true") {
         destination = "Asia";
@@ -20,10 +23,17 @@ $(document).ready(function() {
         alert ("We don't have enough good information to suggest you a destination");
       }
     };
+
     suggestion (kids, animal, activity, passeport);
 
     $("span.name").text(name);
     $("span.destination").text(destination);
+
+  });
+
+  $("#repeat").click(function() {
+    $("#suggestion").hide();
+    $("form").show();
 
   });
 });
